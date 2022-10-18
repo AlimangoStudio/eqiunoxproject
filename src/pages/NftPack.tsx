caecc7da0f14a2a4aeaeb1d84acd38eb2356edbd
a-nickol <a.nickol@trust-nickol.de>
2022-10-18 21:06:46 +0200

    wireframe layout

--- src/pages/NftPack.tsx
+++ src/pages/NftPack.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import PageTitle from '../components/Typography/PageTitle';
+
+import booster from '../assets/img/booster.jpg';
+import { Button } from '@windmill/react-ui';
 
 function NftPack() {
   return (
     <>
       <PageTitle>NFT Pack Mint Page</PageTitle>
+      <div className="pt-8 grid gap-6 grid-cols-2">
+        <div className="flex justify-center">
+          <div className="w-1/2">
+            <img
+              aria-hidden="true"
+              className="object-cover w-full h-full"
+              src={booster}
+              alt="NFT Pack"
+            />
+          </div>
+        </div>
+        <div className="flex items-center">
+          <Button>
+            Mint NFT Pack
+          </Button>
+        </div>
+      </div>
     </>
   );
 };
